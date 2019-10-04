a27e6960389282c40d9c810769e56f970c1f5d41
rajkanya26saha <rajkanya.nhk@gmail.com>
2019-10-04 09:55:55 +0530

    unix_udp programming

---
+++ socket/AF_UNIX_with_SOCK_DGRAM/server.h
@@ -0,0 +1,40 @@
+
+/************************************************************/
+/*Author	: Rajkanya Saha		*/
+/*Date		: 03-oct-2019		*/
+/*Filename	: server.h	*/
+/*Description	: header files for server application*/
+/************************************************************/
+
+#ifndef SERVER_H
+#define SERVER_H
+
+/* Included header files */
+
+#include<sys/types.h> //socket,bind,recvfrom,sendto
+#include<sys/socket.h> //socket,bind,recvfrom,sendto
+#include<stdio.h> 	//remove
+#include<string.h> 	//memset
+#include<unistd.h> 	//close,sleep
+#include<sys/un.h> 
+#include<error.h>
+
+
+
+/* function prototypes */
+
+
+
+
+/* Macro Definitions */
+
+
+
+
+
+/* user defined datatypes definitions */
+
+
+
+
+#endif //SERVER_H
